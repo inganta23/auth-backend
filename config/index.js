4f9d94cdadfbbff65a28f240e6b5bcb62694fa5e
Johanes Inganta Karo-Karo <johanes.karo@bithealth.co.id>
2024-05-09 08:51:41 +0700

    Initial commit

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+module.exports = {
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientUrl: process.env.CLIENT_URL
+};
---
+++ passport.js
@@ -0,0 +1,42 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const passport = require("passport");
+const config = require("./config");
+
+const GOOGLE_CLIENT_ID = config.googleClientId;
+const GOOGLE_CLIENT_SECRET = config.googleClientSecret;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: "386805881017470",
+      clientSecret: "dd2de6f1dd64f5a38712a3b685b2a0b0",
+      callbackURL: "/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
